c67a3bc2fc5064db016278f7a0ae02ee147ab77d
jasonnesbitt-sp <jnesbitt@meowijuana.com>
2026-06-24 08:46:25 -0500

    Remove legacy 'Start Meowi Editor.bat' (superseded by Start SmarterClip Editor.bat)

--- Start Meowi Editor.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title SmarterClip Editor
-cd /d "%~dp0"
-echo Starting SmarterClip Editor...
-echo Opening http://127.0.0.1:8765/ in your browser.
-echo Keep this window open while you edit. Close it to stop the editor.
-start "" http://127.0.0.1:8765/
-python editor\server.py
-pause
